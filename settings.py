a529f0dfc3804c06e16453da17deab97d3e59f53
paulbg <paulbg@hey.com>
2024-11-12 10:56:07 +0200

    add gitignore, remove env

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9f0ces70_b))u^p!05!08o9^5sygo5rd*g-4=6rn*p5z$(alc6'
+SECRET_KEY = config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool) 
